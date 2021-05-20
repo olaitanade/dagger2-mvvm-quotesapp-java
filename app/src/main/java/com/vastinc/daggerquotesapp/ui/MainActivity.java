3846c3457280e7f541cd55578cee02ce2f298c4e
Adetayo Olaitan <olaitan.adetayo.com>
2021-05-20 10:24:35 +0100

    added the navigation component

--- app/src/main/java/com/vastinc/daggerquotesapp/ui/MainActivity.java
+++ app/src/main/java/com/vastinc/daggerquotesapp/ui/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.bumptech.glide.RequestManager;
 import com.vastinc.daggerquotesapp.BaseApplication;
 import com.vastinc.daggerquotesapp.R;
+import com.vastinc.daggerquotesapp.data.remote.model.Quote;
 import com.vastinc.daggerquotesapp.di.main.MainComponent;
+import com.vastinc.daggerquotesapp.util.Resource;
 import com.vastinc.daggerquotesapp.viewmodel.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -35,7 +38,26 @@ public class MainActivity extends AppCompatActivity {
         mainComponent.inject(this);
 
         setContentView(R.layout.activity_main);
-
+        observers();
         //mainActivityViewModel = ViewModelProviders.of(this,providerFactory).get(MainActivityViewModel.class);
+    }
+
+    void observers(){
+        mainActivityViewModel.getRandomQuote("en").observe(this, new Observer<Resource<Quote>>() {
+            @Override
+            public void onChanged(Resource<Quote> quoteResource) {
+                if(quoteResource!=null){
+                    switch (quoteResource.status){
+                        case LOADING:
+                            break;
+                        case SUCCESS:
+                            Log.d(TAG, "onChanged: successful => "+quoteResource.data.getText());
+                            break;
+                        case ERROR:
+                            break;
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/vastinc/daggerquotesapp/ui/MainActivityViewModel.java
+++ app/src/main/java/com/vastinc/daggerquotesapp/ui/MainActivityViewModel.java
@@ -2,18 +2,76 @@ package com.vastinc.daggerquotesapp.ui;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.vastinc.daggerquotesapp.data.remote.main.MainApi;
+import com.vastinc.daggerquotesapp.data.remote.model.Quote;
 import com.vastinc.daggerquotesapp.di.scope.ActivityScope;
+import com.vastinc.daggerquotesapp.util.Resource;
 
 import javax.inject.Inject;
+
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 @ActivityScope
 public class MainActivityViewModel extends ViewModel {
     private static final String TAG = "MainActivityViewModel";
+    private final MainApi mainApi;
+    private MediatorLiveData<Resource<Quote>> randomQuote;
     @Inject
     public MainActivityViewModel(MainApi mainApi){
         Log.d(TAG, "MainActivityViewModel: created");
+        this.mainApi = mainApi;
+    }
+
+    public LiveData<Resource<Quote>> getRandomQuote(String language){
+        if(randomQuote == null){
+            randomQuote = new MediatorLiveData<>();
+            randomQuote.setValue(Resource.loading((Quote) null));
+
+            final LiveData<Resource<Quote>> source = LiveDataReactiveStreams.fromPublisher(
+
+                    mainApi.getRandomQuote(language)
+
+                            .onErrorReturn(new Function<Throwable, Quote>() {
+                                @Override
+                                public Quote apply(Throwable throwable) throws Exception {
+                                    Log.e(TAG, "apply: ", throwable);
+                                    Quote quoteError = new Quote();
+                                    quoteError.setUuid("-1");
+                                    return quoteError;
+                                }
+                            })
+
+                            .map(new Function<Quote, Resource<Quote>>() {
+                                @Override
+                                public Resource<Quote> apply(Quote q) throws Exception {
+
+                                        if(q.getUuid().equals("-1")){
+                                            return Resource.error("Something went wrong", null);
+                                        }
+
+
+                                    return Resource.success(q);
+                                }
+                            })
+
+                            .subscribeOn(Schedulers.io())
+            );
+
+            randomQuote.addSource(source, new Observer<Resource<Quote>>() {
+                @Override
+                public void onChanged(Resource<Quote> quoteResource) {
+                    randomQuote.setValue(quoteResource);
+                    randomQuote.removeSource(source);
+                }
+            });
+        }
+        return randomQuote;
     }
 }
